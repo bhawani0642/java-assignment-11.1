f2ba2d163902d0611b54208516f36fa01ec34299
bhawani0642 <bhawani0642@gmail.com>
2017-06-14 14:59:06 +0530

    Exception

---
+++ src/com/prienc/Exception.java
@@ -0,0 +1,12 @@
+package com.prienc;
+
+public class Exception {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]myarray=  new int[]{1,2,3,4,5,};
+		System.out.println(myarray[5]);
+	}
+ 
+}
